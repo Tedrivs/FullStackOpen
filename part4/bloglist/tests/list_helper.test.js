7695a39cddaffa65c61ddeae66d2998c9a58ef95
Lars Simon Markhus <lars.simon.markhus@webstep.no>
2024-02-27 09:16:56 +0100

    Exercise 4.4

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -8,3 +8,27 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const blogs = []
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog equal the likes of that', () => {
+    const blogs = [{ title: 'Test', author: 'String', url: 'String', likes: 3 }]
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 3)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const blogs = [
+      { title: 'Test', author: 'String', url: 'String', likes: 3 },
+      { title: 'Test', author: 'String', url: 'String', likes: 4 },
+      { title: 'Test', author: 'String', url: 'String', likes: 5 },
+    ]
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 12)
+  })
+})
